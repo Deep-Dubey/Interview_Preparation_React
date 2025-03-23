00b511fc4c8f87a40229ea85970d79d926c6ea0f
DEEP DUBEY <64470309+Deep-Dubey@users.noreply.github.com>
2025-03-23 22:08:28 +0530

    UseFetch how it works

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,6 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-const useFetch = (url, option = { method: "GET" }) => {
+// const useFetch = (url, option = { method: "GET" }) => {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const responce = await fetch(url, { ...option });
+//         if (!responce.ok) {
+//           throw new Error("Network Error");
+//         }
+//         const result = await responce.json();
+//         setData(result);
+//       } catch (error) {
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     if (url) {
+//       fetchData();
+//     }
+//   }, [url, option]);
+//   return { data, loading, error };
+// };
+
+// export default useFetch;
+
+const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -9,7 +39,7 @@ const useFetch = (url, option = { method: "GET" }) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const responce = await fetch(url, { ...option });
+        const responce = await fetch(url);
         if (!responce.ok) {
           throw new Error("Network Error");
         }
@@ -24,7 +54,7 @@ const useFetch = (url, option = { method: "GET" }) => {
     if (url) {
       fetchData();
     }
-  }, [url, option]);
+  }, [url]);
   return { data, loading, error };
 };
 
